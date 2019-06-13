0835456dfa4c455ec471a7785dc6f9b1fe0cd299
Shady Samuel <shadyuop@gmail.com>
2019-06-14 01:43:51 +0200

    Awt Tokens strategy added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://shady:shandra@devconnector-vunth.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://shady:shandra@devconnector-vunth.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -56,5 +56,70 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login user / Returning the token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({
+      email
+    })
+    .then(user => {
+      // check for user
+      if (!user) {
+        return res.status(404).json({
+          email: 'user not found'
+        });
+      }
+
+      //Check Password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            // User Matched
+
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            } // for jwt payload
+
+            // Sign Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey, {
+                expiresIn: 3600
+              },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              }
+            );
+          } else {
+            return res.status(400).json({
+              password: 'Password incorrect'
+            });
+          }
+        });
+    })
+});
+
+// @route   GET api/users/current
+// @desc    current user 
+// @access  Private
+router.get('/current', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email,
+  });
+});
 
 module.exports = router;
