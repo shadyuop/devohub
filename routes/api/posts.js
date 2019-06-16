13a82530e09521046de0eb185bf2b1883d626976
Shady Samuel <shadyuop@gmail.com>
2019-06-16 16:44:37 +0200

    Adding Posts posting and getting routes, validations and Schema

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
+
+
+// Validation
+const ValidatePostInput = require('../../validation/post.js');
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
+router.get('/test', (req, res) => res.json({
+  msg: 'Posts Works'
+}));
+
+// @route   Get api/posts
+// @desc    Get post
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({
+      date: -1
+    })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({
+      nopostfound: 'No Posts found'
+    }));
+});
+
+// @route   Get api/post/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({
+      nopostfound: 'No Post found with that id'
+    }));
+});
+
+// @route   Post api/posts
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const {
+    errors,
+    isValid
+  } = ValidatePostInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  });
+
+  newPost.save().then(post => res.json(post));
+});
+
 
 module.exports = router;
