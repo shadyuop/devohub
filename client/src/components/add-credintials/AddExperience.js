87a3ebd60050aa1055f89b673dcc682ac916c969
Shady Samuel <shadyuop@gmail.com>
2019-06-20 16:52:02 +0200

    Adde Experience functionality

--- client/src/components/add-credintials/AddExperience.js
+++ client/src/components/add-credintials/AddExperience.js
@@ -4,6 +4,7 @@ import { Link, withRouter } from "react-router-dom";
 import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaGroup from "../common/TextAreaGroup";
 import PropTypes from "prop-types";
+import { addExperience } from "../../actions/profileActions";
 
 class AddExperience extends Component {
   constructor(props) {
@@ -25,6 +26,12 @@ class AddExperience extends Component {
     this.onCheck = this.onCheck.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -33,14 +40,17 @@ class AddExperience extends Component {
     e.preventDefault();
     console.log("Submit");
 
-    // const profileData = {
-    //   handle: this.state.handle,
-    //   company: this.state.company,
-    //   website: this.state.website,
-    //   location: this.state.location,
-    // };
+    const expData = {
+      title: this.state.title,
+      company: this.state.company,
+      from: this.state.from,
+      location: this.state.location,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description
+    };
 
-    // this.props.createProfile(profileData, this.props.history);
+    this.props.addExperience(expData, this.props.history);
   }
 
   onCheck(e) {
@@ -144,6 +154,7 @@ class AddExperience extends Component {
 }
 
 AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
 };
@@ -155,5 +166,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {}
+  { addExperience }
 )(withRouter(AddExperience));
