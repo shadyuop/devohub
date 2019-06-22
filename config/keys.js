90d49b2df0636d9d716a17c2b98aef19df6f4967
Shady Samuel <shadyuop@gmail.com>
2019-06-22 21:17:53 +0200

    prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -3,8 +3,14 @@
 //     "mongodb+srv://shady:shandra@devhub-bordi.mongodb.net/test?retryWrites=true&w=majority",
 //   secretOrKey: "secret"
 // };
-module.exports = {
-  mongoURI:
-    "mongodb+srv://shady:shandra@devconnector-vunth.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+// module.exports = {
+//   mongoURI:
+//     "mongodb+srv://shady:shandra@devconnector-vunth.mongodb.net/test?retryWrites=true&w=majority",
+//   secretOrKey: "secret"
+// };
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
